d71afdedd560a4d9c0cfd1527267ae5b0433e59e
Felix Rosén <felix.m.rosen@gmail.com>
2018-03-02 17:44:03 +0100

    add

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
 # load packages
-from flask import Flask, render_template
+import os
+import sqlite3
+from flask import Flask, render_template, url_for, request
+from contextlib import closing
 
 # init app
 app = Flask(__name__)
 
 #routes
 @app.route('/')
-@app.route('/index.html', methods=['GET', 'POST'])
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/submited.html')
-def submited():
-    return "hej"
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        try:
+            con = sqlite3.connect("database.db")
+            try:
+                date = request.form['date']
+                vocation = request.form['vocation']
+                cur = con.cursor()
+                cur.execute("INSERT INTO schedule (date, vocation) values (?,?)", (date, vocation) )
+                con.commit()
+                state = 0
+            except Exception as e: 
+                con.rollback()
+                state = 1
+        except Exception as e: 
+            state = 1
+        finally:
+            return render_template("index.html", state = state)
+    else:
+        print(request.form['date'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,6 @@
+drop table if exists schedule;
+create table schedule (
+	date text not null,
+	vocation text not null
+);
+	
